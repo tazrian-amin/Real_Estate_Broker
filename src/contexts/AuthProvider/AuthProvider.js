70ef282078fea95b7ebe28930fe19df325c22454
tazrian-amin <amin.tazrian1979@gmail.com>
2022-11-21 21:26:33 +0600

    added firebase config, contexts, main layout, along with routes and private route and more

---
+++ src/contexts/AuthProvider/AuthProvider.js
@@ -0,0 +1,34 @@
+import { createContext, useEffect, useState } from "react";
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import app from "../../firebase/firebase.config";
+
+export const AuthContext = createContext();
+const auth = getAuth(app);
+
+const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, currentUser => {
+            setUser(currentUser);
+            setLoading(false);
+        });
+        return () => {
+            return unsubscribe();
+        }
+    }, [])
+
+    const authInfo = {
+        user,
+        loading
+    }
+
+    return (
+        <AuthContext.Provider value={authInfo}>
+            {children}
+        </AuthContext.Provider>
+    )
+};
+
+export default AuthProvider;
