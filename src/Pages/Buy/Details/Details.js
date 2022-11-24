292e5a6899e63eb76f44ccdb31ba657a5025adfd
tazrian-amin <amin.tazrian1979@gmail.com>
2022-11-24 23:30:57 +0600

    added selling section in homepage

--- src/Pages/Buy/Details/Details.js
+++ src/Pages/Buy/Details/Details.js
@@ -15,8 +15,8 @@ const Details = () => {
     return (
         <div>
             <div className="bg-base-100">
-                <h2 className="text-5xl font-semibold text-center mt-3">Buy a perfect {category} that suits you</h2>
-                <div className='mt-7 flex justify-center'>
+                <h2 className="text-5xl font-semibold text-center my-10">Buy a perfect {category} that suits you</h2>
+                <div className='flex justify-center'>
                     <img className='' src={img} alt="" />
                 </div>
                 <PhotoSlider
@@ -34,24 +34,24 @@ const Details = () => {
                     onIndexChange={setIndex}
                 />
 
-                <div className="flex flex-col mt-8 w-full border-opacity-50">
-                    <div className="grid card py-4 bg-base-200 rounded-box place-items-center">
+                <div className="flex flex-col mt-10 w-4/5 mx-auto border-opacity-50 text-center">
+                    <div className="grid card p-4 bg-base-200 rounded-box place-items-center">
                         <h3 className='text-3xl font-bold pb-2'>{category} Details</h3>
-                        <p className='text-lg font-semibold'><FontAwesomeIcon icon={faBed} /> Bed: {details[0].bed}</p>
-                        <p className='text-lg font-semibold'><FontAwesomeIcon icon={faBathtub} /> Bath: {details[0].bath}</p>
-                        <p className='text-lg font-semibold'><FontAwesomeIcon icon={faPlusCircle} /> Facility: {details[0].facility}</p>
-                        <p className='text-lg font-semibold'><FontAwesomeIcon icon={faAreaChart} /> Area: {details[0].area} sqr ft.</p>
-                        <p className='text-lg font-semibold'><FontAwesomeIcon icon={faMapMarkerAlt} /> Location: {location}</p>
-                        <p className='text-lg font-semibold'><FontAwesomeIcon icon={faDollar} /> Price: ${price}</p>
-                        <p className='text-lg font-semibold'><FontAwesomeIcon className='text-warning' icon={faStar} /> Ratings: {rating}</p>
+                        <p className='text-md lg:text-lg font-semibold'><FontAwesomeIcon icon={faBed} /> Bed: {details[0].bed}</p>
+                        <p className='text-md lg:text-lg font-semibold'><FontAwesomeIcon icon={faBathtub} /> Bath: {details[0].bath}</p>
+                        <p className='text-md lg:text-lg font-semibold'><FontAwesomeIcon icon={faPlusCircle} /> Facility: {details[0].facility}</p>
+                        <p className='text-md lg:text-lg font-semibold'><FontAwesomeIcon icon={faAreaChart} /> Area: {details[0].area} sqr ft.</p>
+                        <p className='text-md lg:text-lg font-semibold'><FontAwesomeIcon icon={faMapMarkerAlt} /> Location: {location}</p>
+                        <p className='text-md lg:text-lg font-semibold'><FontAwesomeIcon icon={faDollar} /> Price: ${price}</p>
+                        <p className='text-md lg:text-lg font-semibold'><FontAwesomeIcon className='text-warning' icon={faStar} /> Ratings: {rating}</p>
                     </div>
                     <div className="divider my-10"><button className='btn btn-primary w-100 mx-auto' onClick={() => setVisible(true)} primary>View Gallery</button></div>
-                    <div className="grid card py-4 bg-base-200 rounded-box place-items-center">
+                    <div className="grid card p-4 bg-base-200 rounded-box place-items-center">
                         <h3 className='text-3xl font-bold'>Owner Details</h3>
                         <img className="mask mask-decagon w-32" src={owner[0].photo} alt="owner" />
-                        <p className='text-lg font-semibold'><FontAwesomeIcon icon={faUserAlt} /> Name: {owner[0].name}</p>
-                        <p className='text-lg font-semibold'><FontAwesomeIcon icon={faPhone} /> Phone: {owner[0].phone}</p>
-                        <p className='text-lg font-semibold'><FontAwesomeIcon icon={faEnvelope} /> Email: {owner[0].email}</p>
+                        <p className='text-md lg:text-lg font-semibold'><FontAwesomeIcon icon={faUserAlt} /> Name: {owner[0].name}</p>
+                        <p className='text-md lg:text-lg font-semibold'><FontAwesomeIcon icon={faPhone} /> Phone: {owner[0].phone}</p>
+                        <p className='text-md lg:text-lg font-semibold'><FontAwesomeIcon icon={faEnvelope} /> Email: {owner[0].email}</p>
                     </div>
                 </div>
                 <div className="card-body">
--- src/Pages/Home/ServiceOverview/ServiceOverview.js
+++ src/Pages/Home/ServiceOverview/ServiceOverview.js
@@ -12,7 +12,8 @@ const ServiceOverview = () => {
     }, [])
 
     return (
-        <div className='my-20'>
+        <div className='bg-base-200 w-4/5 mx-auto rounded-md my-10'>
+            <h1 className="text-5xl font-bold text-center py-5">Purchase a Home!</h1>
             <div className='grid grid-cols-1 lg:grid-cols-3'>
                 {
                     services.map(service => <ServiceCard
@@ -21,8 +22,8 @@ const ServiceOverview = () => {
                     ></ServiceCard>)
                 }
             </div>
-            <div className='mt-10 flex justify-center'>
-                <Link to='/buy' className="btn btn-primary">View All</Link>
+            <div className='flex justify-center'>
+                <Link to='/buy' className="btn btn-primary my-10">View All</Link>
             </div>
         </div>
     );
